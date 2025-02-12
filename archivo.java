d97eb8e650f77e073ed702054896e12803d96d90
Ibai <ikimatarranz24@lhusurbil.eus>
2025-02-12 20:10:21 +0100

    añadir archivos ignorando py

---
+++ archivo.java
@@ -0,0 +1,5 @@
+public class Archivo {
+    public static void main(String[] args) {
+        System.out.println("Hola, este es archivo.java");
+    }
+}
---
+++ archivo3.java
@@ -0,0 +1,5 @@
+public class Archivo3 {
+    public static void main(String[] args) {
+        System.out.println("Este es archivo3.java");
+    }
+}
